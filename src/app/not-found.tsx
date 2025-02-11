9dfdc11a94c37c9d9ec5fa5aab2405abb10c1cf5
Emanuel Molina <emmanueljose.molina@salud.gob.sv>
2025-02-11 15:17:57 -0600

    add page not found custom

---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@/components/ui/button'
+import { TriangleAlert } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <div className='bg-white p-10 shadow rounded-lg m-10 flex items-center flex-col'>
+      <div className='flex items-center gap-3'>
+        <h2 className='text-4xl font-bold'>Not Found</h2>
+        <TriangleAlert size={44}/>
+      </div>
+      <p className='text-lg'>No se encontro el recurso solicitado</p>
+      
+      <Image src="/bob_esponja_cavernicola.png" alt='Bob Esponja Cavernicola' width={500} height={500}></Image>
+
+      <Button asChild>
+        
+        <Link href="/">Regresar al Inicio</Link>
+      </Button>
+      
+    </div>
+  )
+}
