fdac026ef4be1e188e58ca4498fa087c4f9d7244
Emanuel Molina <emmanueljose.molina@salud.gob.sv>
2025-02-11 13:58:54 -0600

    refactor NavBreadcrumb component to simplify breadcrumb generation and improve readability

--- src/app/components/NavBreadcrumb.tsx
+++ src/app/components/NavBreadcrumb.tsx
@@ -5,52 +5,11 @@ import { House } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Fragment } from "react";
-import { menu as menuList } from "@/utils/menu"
-
-interface SubMenu{
-    title: string;
-    url: string;
-}
-
-interface Menu {
-    title: string;
-    url: string;
-    items: SubMenu[] | null;
-}
+import { capitalizeFirstLetter } from "@/utils/functions";
 
 export default function NavBreadcrumb() {
     const path = usePathname();
     const breadcrumbList = path.split('/').filter(Boolean);
-
-    const getNamesPath = (itemsBreadcrumb : string[]) => {
-        const menuItems: Menu[] = [];
-        
-        itemsBreadcrumb.forEach((item, index) => {
-            if (index === 0) {
-                const menu = menuList.find(menu => menu.url === `/${item}`);
-                menuItems.push({
-                    title: menu!.title,
-                    url: menu!.url,
-                    items: menu!.items
-                });
-            } else {
-                if(menuItems[0].items !== null){
-                    
-                    const subMenu = menuItems[0]!.items.find(subItem => subItem.url === `/${item}`);
-                    
-                    menuItems.push({
-                        title: subMenu!.title,
-                        url: `${menuItems[0].url}${subMenu!.url}`,
-                        items: null
-                    });
-                }
-            }
-        });
-
-        return menuItems
-    }
-
-    const menuses = getNamesPath(breadcrumbList);
     
     return (
         <Breadcrumb className="mb-4">
@@ -62,15 +21,16 @@ export default function NavBreadcrumb() {
                 </BreadcrumbPage>
                 <BreadcrumbSeparator/>
                 {
-                    menuses.map((item, index) => {
+                    breadcrumbList.map((item, index) => {
+                        const url = `/${breadcrumbList.slice(0, index + 1).join("/")}`;
                         return (
                             <Fragment key={index}>
                                 <BreadcrumbItem >
                                     <BreadcrumbPage>
-                                        <Link href={item.url}>{item.title}</Link>
+                                        <Link href={url}>{capitalizeFirstLetter(item)}</Link>
                                     </BreadcrumbPage>
                                 </BreadcrumbItem>
-                                { index < menuses.length - 1 && <BreadcrumbSeparator/> }
+                                { index < breadcrumbList.length - 1 && <BreadcrumbSeparator/> }
                             </Fragment>
                         )
                     })
