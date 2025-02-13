4e95e3cc8d5243d92fa3c27e8337a76f47ba254b
Emanuel Molina <emmanueljose.molina@salud.gob.sv>
2025-02-13 15:30:41 -0600

    add view show role

---
+++ src/app/administration/roles/show/[id]/page.tsx
@@ -0,0 +1,60 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
+import { Title } from '@/components/ui/title'
+import { PrismaClient } from '@prisma/client'
+import Link from 'next/link'
+
+const prisma = new PrismaClient()
+
+export default async function RolShow({params}: {params : Promise<{ id: string}>}) {
+    const id = parseInt((await params).id)
+    const rol = await prisma.roles.findUnique({where:{ id}});
+    await prisma.$disconnect();
+
+    return (
+      <>
+        <Title>Detalles rol</Title>
+
+          <Card>
+            <CardHeader>
+                <CardDescription>Detalles del rol </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className='grid grid-cols-12 gap-6'>
+                  <div className='col-span-12 lg:col-span-6'>
+                    <Label>Nombre</Label>
+                    <Input disabled value={rol?.name}></Input>
+                  </div>
+
+                  <div className='col-span-12 lg:col-span-6'>
+                    <Label>Descripción</Label>
+                    <Textarea disabled>{rol?.description}</Textarea>
+                  </div>
+                  
+
+                  <div className='col-span-12 lg:col-span-6'>
+                    <Label>Fecha de creación</Label>
+                    <Input disabled value={rol?.name}></Input>
+                  </div>
+
+                  <div className='col-span-12 lg:col-span-6'>
+                    <Label>Fecha de modificion</Label>
+                    <Input disabled value={rol?.name}></Input>
+                  </div>
+
+                  {/* TODO: motrar menus asignados a ese rol */}
+                
+                </div>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <Button asChild variant="outline">
+                  <Link href='/administration/roles'>Regresar</Link>
+                </Button>
+            </CardFooter>
+          </Card>
+      </>
+    )
+}
