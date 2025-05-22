3e242c4f46cfef0667404682ae33ce3609d30c88
Emanuel Molina <emmanueljose.molina@salud.gob.sv>
2025-05-22 13:10:33 -0600

    fifix filter roles

--- src/app/administration/access/add/page.tsx
+++ src/app/administration/access/add/page.tsx
@@ -3,7 +3,13 @@ import FormAddAccess from "./components/FormAddAccess";
 
 export default async function UsersAdd() {
 
-    const roles = await prisma.roles.findMany()
+    const roles = await prisma.roles.findMany({
+        where:{
+            id:{
+                notIn: [1]
+            }
+        }
+    })
     const menus = await prisma.menus.findMany({
         where:{
             menu: true,
