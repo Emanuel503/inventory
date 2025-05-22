828df5ede07b5c48ed2b0a21f47454badc4f05e7
Emanuel Molina <emmanueljose.molina@salud.gob.sv>
2025-05-22 14:11:51 -0600

    add page setting general

---
+++ src/app/settings/general/page.tsx
@@ -0,0 +1,8 @@
+import { Title } from '@/components/ui/title'
+import React from 'react'
+
+export default function GeneralSettingPage() {
+  return (
+    <Title>Configuración General</Title>
+  )
+}
