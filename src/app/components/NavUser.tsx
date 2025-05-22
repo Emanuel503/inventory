ab614b7a7b4691f23f7d59dc29f91fd9359bc06e
Emanuel Molina <emmanueljose.molina@salud.gob.sv>
2025-05-22 12:16:34 -0600

    fix user Avatar

--- src/app/components/NavUser.tsx
+++ src/app/components/NavUser.tsx
@@ -21,7 +21,7 @@ export function NavUser({ user } : {user : Users}) {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={""} alt={`${user.names} ${user.surnames}`} />
+                <AvatarImage src="https://petsastherapy.org/images/uploads/cutouts/Cats_for_website_2.0_copy_.png" alt={`${user.names} ${user.surnames}`} />
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
@@ -40,7 +40,7 @@ export function NavUser({ user } : {user : Users}) {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={""} alt={`${user.names} ${user.surnames}`} />
+                  <AvatarImage src="https://petsastherapy.org/images/uploads/cutouts/Cats_for_website_2.0_copy_.png" alt={`${user.names} ${user.surnames}`} />
                   <AvatarFallback className="rounded-lg">SI</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
