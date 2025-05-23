3212d9ebc090414a3ad3b90e109c5d4a91904db5
Emanuel Molina <emanueljosemolina@gmail.com>
2025-05-22 22:02:00 -0600

    add description to settings

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,7 +1,13 @@
 import { Title } from '@/components/ui/title'
+import { Separator } from "@/components/ui/separator"
 
 export default function AccountPage() {
   return (
-    <Title>Cuenta</Title>
+    <>
+      <Title>Cuenta</Title>
+      <p className='mb-5'>Maneja la configuración de tu cuenta y preferencias</p>
+
+      <Separator className='mb-5'/>
+    </>
   )
 }
