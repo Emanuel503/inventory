e9d056a01a8d4dfe5a0bbfb6d8ed7f6674645d36
Emanuel Molina <emanueljosemolina@gmail.com>
2025-05-25 21:56:02 -0600

    add sessions layout

--- src/app/account/sessions/page.tsx
+++ src/app/account/sessions/page.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { Title } from "@/components/ui/title";
+import { prisma } from "@/utils/prisma";
 
-export default function SessionsPage() {
+export default async function SessionsPage() {
+
+  const sessions = await prisma.sessions.findMany({
+    where: {
+      idUser: 1
+    },
+    orderBy: {
+      id: "asc"
+    }
+  });
+
+  //TODO: Agregar condicion para que solo muestre todas las sesiones activas, las ultimas 5 expiradas y las ultimas 5 cerradas
+  //TODO: utilizar un acordeon para mostrar las sesiones
+
   return (
-    <div>Sessions</div>
+    <>
+      <Title>Sesiones</Title>
+
+      <p className="text-sm text-muted-foreground">
+        Aquí puedes ver los dispositivos y ubicaciones donde has iniciado sesión. Si ves algo sospechoso, puedes cerrar la sesión de ese dispositivo.
+      </p>
+      
+      <div className="mb-10">
+        {
+          sessions.map((session) => (
+            <div key={session.id} className="border-b border-gray-200 py-4">
+              <p className="text-sm font-medium">{session.device}</p>
+              <p className="text-xs text-muted-foreground">{session.ipAddress}</p>
+              
+            </div>
+          ))
+        }
+      </div>
+
+      <p className="text-sm text-muted-foreground">
+        Si necesitas ayuda, contacta a soporte.
+      </p>
+    </>
   )
 }
